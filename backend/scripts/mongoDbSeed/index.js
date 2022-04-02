e254c6b36e07f0a83ae9e832e4642da7ff43a745
Omer Salomon <steinerhunter@gmail.com>
2022-04-02 17:20:38 +0300

    Ok now I've finally figured it out :)

--- backend/scripts/mongoDbSeed/index.js
+++ backend/scripts/mongoDbSeed/index.js
@@ -9,33 +9,20 @@ function randomIntFromInterval(min, max) { // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-async function seedDB() {
-  // Connection URL
-  const uri = process.env.MONGODB_URI
+function genRanHex(size) {
+  return [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+}
 
-  const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    // useUnifiedTopology: true,
-  });
-
-  try {
-
-    const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
-
-    await client.connect();
-    console.log("Connected correctly to server");
-
-    const collection = client.db("config").collection("users");
-
-    const insertQuery = [];
-
-    for (let i = 0; i < 300; i++) {
-      const randomStr = genRanHex(32);
+function generateDocument(collection_name) {
+  let randomStr;
+  const date = faker.date.past();
+  switch(collection_name) {
+    case 'users':
+      randomStr = genRanHex(32);
       const randomHash = genRanHex(1024);
-      const date = faker.date.past();
       const firstName = faker.name.firstName();
       const lastName = faker.name.lastName();
-      const newUser = {
+      return {
         role: 'user',
         favorites: [],
         following: [],
@@ -46,17 +33,68 @@ async function seedDB() {
         createdAt: date,
         updatedAt: date,
         __v: 0
-      }
-
-      insertQuery.push(newUser);
+      };
+    case 'items':
+      randomStr = (Math.random() + 1).toString(36).substring(7);
+      const randomTitle = faker.commerce.product();
+      return {
+        favoritesCount: 0,
+        comments: [],
+        tagList: [],
+        title: randomTitle,
+        description: faker.hacker.phrase(),
+        image: faker.image.avatar(),
+        seller: ObjectId("622b8e1739c8fd4464700dd5"),
+        slug: `${randomTitle}-${randomStr}`,
+        createdAt: date,
+        updatedAt: date,
+        __v: 0
+      };
+    case 'comments':
+    {
+      return {
+        body: faker.hacker.phrase(),
+        item: ObjectId("622d0842e78e92d45a55b846"),
+        seller: ObjectId("622b8e1739c8fd4464700dd5"),
+        createdAt: date,
+        updatedAt: date,
+        __v: 0
+      };
     }
-    await collection.insertMany(insertQuery);
+    default:
+      return {};
+  }
+}
 
-    console.log("Database seeded! :)");
-    await client.close();
+async function seedDB() {
+  // Connection URL
+  const uri = process.env.MONGODB_URI
+
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  });
+
+  const collection_names = ['users', 'items', 'comments'];
+
+  try {
+    for (const collection_name of collection_names) {
+      await client.connect();
+      console.log("Connected correctly to server");
+      const collection = client.db("anythink-market").collection(collection_name);
+      const insertQuery = [];
+      for (let i = 0; i < 300; i++) {
+        const newDocument = generateDocument(collection_name);
+        insertQuery.push(newDocument);
+      }
+      await collection.insertMany(insertQuery);
+      console.log("Database seeded! :)");
+      await client.close();
+    }
   } catch (err) {
     console.log(err.stack);
   }
+
 }
 
 seedDB();
